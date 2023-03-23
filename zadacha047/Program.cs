c9e2d90076307eea91334285b091a52900234c95
Dmitrii Lih <omts1dm@gmail.com>
2023-03-23 23:58:43 +0500

    задача 47, 50, 52.

--- zadacha047/Program.cs
+++ zadacha047/Program.cs
@@ -1,9 +1,43 @@
 ﻿// Задача 47. Задайте двумерный массив размером m×n, 
 // заполненный случайными вещественными числами.
 // m = 3, n = 4.
-// 0,5 7 -2 -0,2
-// 1 -3,3 8 -9,9
-// 8 7,8 -7,1 9
+// 0,5   7   -2  -0,2
+//  1  -3,3   8  -9,9
+//  8   7,8 -7,1   9
 
 
+Console.Clear();
+
+double[,] FillMatrixRandom(int row, int column)
+{
+    double[,] arr = new double[row, column];
+    
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        Random rnd = new Random();
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = Math.Round(rnd.NextDouble() + rnd.Next(10, 99),1);
+        }
+    }
+    return arr;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i,j] + ";  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+PrintMatrix(FillMatrixRandom(row,column));
 
--- zadacha50/Program.cs
+++ zadacha50/Program.cs
@@ -7,4 +7,83 @@
 //  8 4 2 4
 // 17 -> такого числа в массиве нет
 
+Console.Clear();
 
+int[,] ArrayRandom(int row, int column) 
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(-9, 10);
+        }
+    }
+    return arr;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + ";  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+string SearchByIndexes(int[,] matrix, int idx0, int idx1)
+{
+    if (idx0 >= 0 && idx0 < matrix.GetLength(0)
+        && idx1 >= 0 && idx1 < matrix.GetLength(1))
+    {
+        return $"Там находится число: {matrix[idx0, idx1]}";
+    }
+    else
+    {
+        return "Таких индексов нет в массиве.";
+    }
+}
+
+string SearchForValue(int[,] matrix, int value)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] == value) return $"Число {value} находится: на строке с индексом {i}, в столбце с индексом {j}.";
+        }
+    }
+    return "Такого числа в массиве нет.";
+}
+Console.Write("Введите количество строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = ArrayRandom(row, column);
+PrintMatrix(matrix);
+Console.WriteLine("Введите:\n1 - узнать индекс введенного числа." +
+            "\n2 - узнать число под заданным индексом. ");
+row = Convert.ToInt32(Console.ReadLine());
+switch (row)
+{
+    case 1:
+        Console.Write("Введите число поиска: ");
+        int value = Convert.ToInt32(Console.ReadLine());
+        Console.Write(SearchForValue(matrix, value));
+        break;
+
+    case 2:
+        Console.Write("Введите индекс строки: ");
+        int idxRow = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите индекс столбца: ");
+        int idxColumn = Convert.ToInt32(Console.ReadLine());
+        Console.Write(SearchByIndexes(matrix, idxRow, idxColumn));
+        break;
+    default:
+        Console.Write("Ошибка! Введите только 1 или 2");
+        break;
+}
